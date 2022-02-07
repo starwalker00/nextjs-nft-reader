13a2a4d1d206a54c06639f0956b180134f166abc
starwalker00 <aaronwalkerup@gmail.com>
2022-02-07 11:39:54 +0100

    style nft list

--- components/NFTList.js
+++ components/NFTList.js
@@ -1,19 +1,46 @@
+import styled from 'styled-components';
+const Wrapper = styled.div`
+padding:2em;
+`;
+const List = styled.ul`
+height: 150px;
+width: 100%;
+`;
+const ListItem = styled.li`
+border: 1px dashed red;
+display:flex;
+background: papayawhip;
+overflow;auto;
+`;
+const ImageItem = styled.img`
+height: 150px;
+`;
+
 function NFTList({ nfts }) {
     return (
         <>
             <div>
                 {nfts.totalCount}
             </div>
-            <div>
-                <ul>
+            <Wrapper>
+                <List>
                     {nfts.ownedNfts.map(nft => (
-                        <li key={JSON.stringify(nft)}>
-                            <img src={nft.image} alt={nft.title} />
+                        <ListItem key={JSON.stringify(nft)}>
+                            { // display video or image
+                                nft.video ?
+                                    <video height="150" controls>
+                                        <source src={nft.video} />
+                                    </video>
+                                    :
+                                    <ImageItem src={nft.image} alt={nft.title} />
+                            }
+
+
                             {JSON.stringify(nft)}
-                        </li>
+                        </ListItem>
                     ))}
-                </ul>
-            </div>
+                </List>
+            </Wrapper>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Form from '../components/Form'
 import NFTList from '../components/NFTList'
+import styled from 'styled-components';
 
 import { useRouter } from 'next/router';
 import { createAlchemyWeb3 } from "@alch/alchemy-web3";
 
 let defaultOwnerAddress = "0xfa5d05df712b059b74ccefe4084785be7f2ea1b8";
+
+const Wrapper = styled.main`
+  padding: 4em;
+`;
 
 function Home({ nfts }) {
   const router = useRouter();
@@ -17,10 +22,10 @@ function Home({ nfts }) {
     })
   }
   return (
-    <div>
+    <Wrapper>
       <Form defaultOwnerAddress={defaultOwnerAddress} refreshData={refreshData} />
       <NFTList nfts={nfts} />
-    </div>
+    </Wrapper>
   )
 }
 
@@ -39,7 +44,7 @@ export async function getServerSideProps(context) {
     owner: ownerAddress
   })
   // console.log(nfts);
-  nfts.ownedNfts = nfts.ownedNfts.slice(0, 2); // keep only first 2 NFTs
+  nfts.ownedNfts = nfts.ownedNfts.slice(0, 9); // keep only first 2 NFTs
   // console.log(nfts);
 
   // fill metadatas for each NFT
@@ -54,21 +59,24 @@ export async function getServerSideProps(context) {
     })
     console.log(NftMetadata)
     nfts.ownedNfts[iter].title = NftMetadata.title;
-    nfts.ownedNfts[iter].image = NftMetadata.metadata.image;
-    // nfts.ownedNfts[iter].image = NftMetadata.tokenUri.gateway;
 
-    console.log(nfts.ownedNfts[iter])
+    // testing all cases, clean after everything is clarified
+    if (NftMetadata.metadata.image) {
+      let HTTPUrl = NftMetadata.metadata.image;
+      if (NftMetadata.metadata.image.startsWith('ipfs://')) {
+        HTTPUrl = convertIPFStoHTTP(HTTPUrl);
+      }
+      if (HTTPUrl.endsWith('.webm')) {
+        nfts.ownedNfts[iter].video = HTTPUrl;
+      } else {
+        nfts.ownedNfts[iter].image = HTTPUrl;
+      }
+    }
+    else {
+      nfts.ownedNfts[iter].image = "https://via.placeholder.com/150"
+    }
+    // console.log(nfts.ownedNfts[iter])
   }
-  // NFT Metadata
-  // const response = await web3.alchemy.getNftMetadata({
-  //   contractAddress: "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
-  //   tokenId: "1590"
-  // })
-  // console.log(response);
-  // console.log(response.title);
-  // console.log(response.id.tokenId);
-  // console.log(response.metadata.image);
-  // convertIPFStoHTTP(response.metadata.image);
 
   // Pass data to the page via props
   return { props: { nfts } }
